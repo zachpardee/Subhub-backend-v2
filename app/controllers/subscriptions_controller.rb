b009efbfdf0ba9bb61f7df4d8b2750f4c4af93cd
Zach Pardee <zachpardee@gmail.com>
2019-06-10 10:38:14 -0600

    added permission for user_id in order to post to subs

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -46,6 +46,6 @@ class SubscriptionsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def subscription_params
-      params.require(:subscription).permit(:title, :url, :username, :password, :cost, :decimal, :paydate, :autopay)
+      params.require(:subscription).permit(:title, :url, :username, :password, :cost, :decimal, :paydate, :autopay, :user_id)
     end
 end
